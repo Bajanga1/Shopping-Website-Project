d3b7f265440c92c7da67560983a0f3c015a68c90
steven miller <millersteven@live.com>
2021-03-01 16:59:52 -0500

    Added Copyright

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function Footer() {
     return (
-        <div>
-            <footer>Footer</footer>
-        </div>
+      
+            <footer>
+                <Container>
+                    <Row>
+                        <Col>Copyright &copy; The Treetops</Col>
+                    </Row>
+                </Container>
+            </footer>
+       
     )
 }
 
