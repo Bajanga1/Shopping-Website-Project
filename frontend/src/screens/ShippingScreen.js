b90c4efbae31f017fc537e8c56b5808396017afd
steven miller <millersteven@live.com>
2021-03-07 12:55:03 -0500

    Start of payment page
    This is right before im gonna build out my payment page, i added a nice progress bar to let you know how far along the checkout process is

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,19 +2,30 @@ import React, { useState, useEffect } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
+import { saveShippingAddress } from '../actions/cartActions'
 
 function ShippingScreen(history) {
-    const [address, setAddress] = useState('')
-    const [city, setCity] = useState('')
-    const [postalCode, setPostalCode] = useState('')
-    const [country, setCountry] = useState('')
+
+    const cart = useSelector(state => state.cart)
+    const { shippingAddress } = cart
+
+    const dispatch = useDispatch()
+
+    const [address, setAddress] = useState(shippingAddress.address)
+    const [city, setCity] = useState(shippingAddress.city)
+    const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
+    const [country, setCountry] = useState(shippingAddress.country)
 
     const submitHandler = (event) => {
         event.preventDefault()
-        console.log('submitted')
+        dispatch(saveShippingAddress({address, city, postalCode, country}))
+        history.push('/payment')
     }
+
     return (
         <FormContainer>
+            <CheckoutSteps />
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
 
@@ -34,14 +45,14 @@ function ShippingScreen(history) {
 
                 <Form.Group controlId='postalCode'>
                     <Form.Label>
-                    Postal Code
+                        Postal Code
                     </Form.Label>
                     <Form.Control required type='text' placeholder='Enter Postal Code' value={postalCode ? postalCode : ''} onChange={(event) => setPostalCode(event.target.value)}></Form.Control>
                 </Form.Group>
 
                 <Form.Group controlId='country'>
                     <Form.Label>
-                    Country
+                        Country
                     </Form.Label>
                     <Form.Control required type='text' placeholder='Enter Country' value={country ? country : ''} onChange={(event) => setCountry(event.target.value)}></Form.Control>
                 </Form.Group>
